80974e3a81fb89f0c030cac15846e6170a561504
oscarcema <soel.job.training@gmail.com>
2024-03-05 17:04:43 -0600

    first commit

---
+++ AzureDevOpsGit/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzureDevOpsGit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
